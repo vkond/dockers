668c4c606268c8826520d8f0a8af074cba8c8b92
Vlad Kondratiev <vlad.kondratiev@gmail.com>
2024-06-13 13:26:27 +0200

    Presto is temporary removed from the build

--- psr-deduction/rebuild-all.sh
+++ psr-deduction/rebuild-all.sh
@@ -4,4 +4,4 @@ sudo singularity build -F deduction101.sif deduction101.def
 sudo singularity build -F deduction-pre-pulsar.sif deduction-pre-pulsar.def
 sudo singularity build -F deduction-pulsar.sif deduction-pulsar.def
 sudo singularity build -F deduction-dspsr.sif deduction-dspsr.def
-sudo singularity build -F deduction-presto.sif deduction-presto.def
+#sudo singularity build -F deduction-presto.sif deduction-presto.def
